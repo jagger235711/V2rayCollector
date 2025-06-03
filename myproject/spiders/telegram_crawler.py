ad20c3f53febde9b1f1053510ba5bd5473fec77a
Jagger <>
2025-06-03 19:20:10 +0800

    [dev]添加导入channels文件进行批量爬取逻辑

--- myproject/spiders/telegram_crawler.py
+++ myproject/spiders/telegram_crawler.py
@@ -3,14 +3,17 @@ import re
 import base64
 import json
 import time
+import pandas as pd
 from urllib.parse import urlparse
 from ..items import ProxyItem
 
 class TelegramCrawlerSpider(scrapy.Spider):
     name = "telegram_crawler"
     allowed_domains = ["t.me"]
+    channelsPath='./resource/channels.csv'
     # start_urls = []  # 通过构造函数传入实际URL
-    start_urls = ['https://t.me/s/wxgmrjdcc']  # 通过构造函数传入实际URL
+    # 读取csv文件 将第一列转为list
+    start_urls = pd.read_csv(channelsPath, usecols=[0], header=None)[0].tolist()
     
     custom_settings = {
         'LOG_LEVEL': 'DEBUG',
@@ -84,57 +87,37 @@ class TelegramCrawlerSpider(scrapy.Spider):
     def extract_configs(self, text):
         configs = []
         # 使用go的正则表达式模式
+        # pattern_telegram_user = r'(?:@)(\w{4,})'
+        # pattern_url = r'(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))'
+        # pattern_shadowsocks = r"(?<![\w-])(ss://[^\s<>#]+)"
+        # pattern_trojan = r"(?<![\w-])(trojan://[^\s<>#]+)"
+        # pattern_vmess = r"(?<![\w-])(vmess://[^\s<>#]+)"
+        # pattern_vless = r"(?<![\w-])(vless://(?:(?!=reality)[^\s<>#])+(?=[\s<>#]))"
+        # pattern_reality = r"(?<![\w-])(vless://[^\s<>#]+?security=reality[^\s<>#]*)"
+        # pattern_tuic = r"(?<![\w-])(tuic://[^\s<>#]+)"
+        # pattern_hysteria = r"(?<![\w-])(hysteria://[^\s<>#]+)"
+        # pattern_hysteria_ver2 = r"(?<![\w-])(hy2://[^\s<>#]+)"
+        # pattern_juicity = r"(?<![\w-])(juicity://[^\s<>#]+)"
         regex_patterns = {
-            'ss': r'(?m)(...ss:|^ss:)\/\/.+?(%3A%40|#)',
-            'vmess': r'(?m)vmess:\/\/[A-Za-z0-9+/=]+',
-            'trojan': r'(?m)trojan:\/\/.+?(%3A%40|#)',
-            'vless': r'(?m)vless:\/\/.+?(%3A%40|#)',
-            'hysteria2': r'(?m)hysteria2:\/\/.+?(%3A%40|#)'
+            # 'ss': r'(?m)(?<![\w-])(ss://[^\s<>#]+)',#直接放弃ss节点，可用概率较低
+            'vmess': r'(?m)(?<![\w-])(vmess://[^\s<>#]+)',
+            'trojan': r'(?m)(?<![\w-])(trojan://[^\s<>#]+)',
+            'vless': r'(?m)(?<![\w-])(vless://(?:(?!=reality)[^\s<>#])+(?=[\s<>#]))',
+            'hysteria2': r'(?m)(?<![\w-])(hysteria2://[^\s<>#]+)'
         }
-        # regex_patterns = {
-        #     # SS协议
-        #     'ss': r'(?m)(?:^|[^a-zA-Z0-9])ss:\/\/[A-Za-z0-9+\/=%_-]+(?:@[a-zA-Z0-9_.-]+:[0-9]+)?(?:\?[a-zA-Z0-9%=&_-]+)?(?:#[^\s"\'<>()]+)?',
-
-        #     # VMess协议
-        #     'vmess': r'(?m)(?:^|[^a-zA-Z0-9])vmess:\/\/[A-Za-z0-9+\/=_-]+(?=[^\w\/+]|$)',
-
-        #     # Trojan协议
-        #     'trojan': r'(?m)(?:^|[^a-zA-Z0-9])trojan:\/\/[^@\s]+@[a-zA-Z0-9_.-]+:[0-9]+(?:\?[a-zA-Z0-9%=&_-]+)?(?:#[^\s"\'<>()]+)?',
-
-        #     # VLESS协议
-        #     'vless': r'(?m)(?:^|[^a-zA-Z0-9])vless:\/\/[^@\s]+@[a-zA-Z0-9_.-]+:[0-9]+(?:\?[a-zA-Z0-9%=&_.-]+)?(?:#[^\s"\'<>()]+)?',
-
-        #     # Hysteria2协议
-        #     'hysteria2': r'(?m)(?:^|[^a-zA-Z0-9])hysteria2:\/\/[^@\s]+@[a-zA-Z0-9_.-]+:[0-9]+(?:\?[a-zA-Z0-9%=&_.-]+)?(?:#[^\s"\'<>()]+)?'
-
-        # }
+   
 
         # 实现类似go的ExtractConfig逻辑
         for proto, pattern in regex_patterns.items():
             matches = re.findall(pattern, text)
             for match in matches:
+                if isinstance(match, tuple):  # 处理分组匹配
+                    match = match[0]
                 # 排除长度小于20的匹配
                 if len(match) < 20:
                     continue
-                if isinstance(match, tuple):  # 处理分组匹配
-                    match = match[0]
                 
-                # 处理ss协议的特殊情况
-                if proto == 'ss' and match.startswith('ss://'):
-                    prefix = match.split('ss://')[0]
-                    if prefix and prefix != 'vle':  # 排除vless误匹配
-                        config = f'ss://{match.split("ss://")[1]}'
-                        configs.append({'type': proto, 'config': config})
-                else:
-                    config = match
-                    configs.append({'type': proto, 'config': config})
-
-        # 去重处理
-        unique_configs = []
-        seen = set()
-        for config in configs:
-            if config['config'] not in seen:
-                seen.add(config['config'])
-                unique_configs.append(config)
+                config = match
+                configs.append({'type': proto, 'config': config})
                 
-        return unique_configs
+        return configs
